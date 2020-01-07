fee12e6518e35b62c1e934cfb5769bb0875206a1
ronamit <Was21235>
2020-01-07 18:31:07 +0200

    update linux-4

--- run_net_depth.py
+++ run_net_depth.py
@@ -37,6 +37,7 @@ parser.add_argument('--run-name', type=str, help='Name of dir to save results in
 parser.add_argument('--seed', type=int,  help='random seed', default=1)
 parser.add_argument("--env", default="HalfCheetah-v2")  # OpenAI gym environment name
 parser.add_argument("--default_discount", default=0.999)  # Default Discount factor
+parser.add_argument("--default_L2", default=0.)  # Default Discount factor
 parser.add_argument('--timesteps_total', type=int,  default=1e5)
 parser.add_argument('--learning_starts', type=int,  default=1e4)
 parser.add_argument('--pure_exploration_steps', type=int,  default=1e4)
@@ -50,19 +51,17 @@ local_mode = False   # True/False - run non-parallel to get error messages and d
 save_PDF = False  # False/True - save figures as PDF file
 
 # Option to load previous run results or continue unfinished run or start a new run:
-run_mode = 'Load'   # 'New' / 'Load' / 'Continue'
+run_mode = 'New'   # 'New' / 'Load' / 'Continue'
 # If run_mode ==  'Load' / 'Continue' use this results dir:
 result_dir_to_load = './saved/2020_01_03_15_27_19'
 
 args.n_reps = 100   # 100 # number of experiment repetitions for each point in grid
 
-#  how to create parameter grid:
-# args.param_grid_def = {'type': 'gamma_guidance', 'spacing': 'linspace', 'start': 0.95, 'stop': 0.995, 'num': 10}
-args.param_grid_def = {'type': 'L2_factor', 'spacing': 'linspace', 'start': 0.0, 'stop': 0.02, 'num': 21}
-# args.param_grid_def = {'type': 'L2_factor', 'spacing': 'list', 'list': [0, 1e-5, 2e-5, 3e-5, 4e-5, 5e-5, 1e-4]}
 
-gamma_guidance = args.default_discount# default discount factor for algorithm
-l2_factor = None   # default L2 regularization factor for the Q-networks
+net_depth_grid = [1, 2, 3, 4]
+param_configs_grid = [{'name': 'No Regularizer', 'gamma': 0.999, 'L2': 0.0},
+                      {'name': 'Discount Regularizer', 'gamma': 0.985, 'L2': 0.0},
+                      {'name': 'L2 Regularizer', 'gamma': 0.999, 'L2': 0.006}]
 
 
 if smoke_test:
@@ -81,89 +80,81 @@ if run_mode in {'Load', 'Continue'}:
     args, info_dict = load_run_data(result_dir_to_load)
     mean_R = info_dict['mean_R']
     std_R = info_dict['std_R']
-    alg_param_grid = info_dict['alg_param_grid']
+    net_depth_grid = info_dict['net_depth_grid']
+    param_configs_grid = info_dict['param_configs_grid']
     print('Loaded parameters: \n', args, '\n', '-'*20)
 else:
     # Start from scratch
     create_result_dir(args)
-    alg_param_grid = np.around(get_grid(args.param_grid_def), decimals=10)
-    n_gammas = len(alg_param_grid)
-    mean_R = np.full(n_gammas, np.nan)
-    std_R = np.full(n_gammas, np.nan)
+
+    run_grid_shape = (len(net_depth_grid), len(param_configs_grid))
+
+    mean_R = np.full(run_grid_shape, np.nan)
+    std_R = np.full(run_grid_shape, np.nan)
 
 if run_mode in {'New', 'Continue'}:
     # Run grid
     ray.init(local_mode=local_mode)
     start_time = timeit.default_timer()
 
-    for i_grid, alg_param in enumerate(alg_param_grid):
-        if not np.isnan(mean_R[i_grid]):
-            continue  # this index already completed
-        set_random_seed(args.seed)
+    for i_net_depth,  net_depth in enumerate(net_depth_grid):
+        for i_param_config, param_config in enumerate(param_configs_grid):
 
-        if args.param_grid_def['type'] == 'L2_factor':
-            l2_factor = alg_param
-            run_name = 'L2_' + str(l2_factor)
-        elif args.param_grid_def['type'] == 'gamma_guidance':
-            gamma_guidance = alg_param
-            run_name = 'Gamma_' + str(alg_param)
-        else:
-            raise ValueError('Unrecognized args.grid_type')
+            if not np.isnan(mean_R[i_net_depth, i_param_config]):
+                continue  # this index already completed
 
-        write_to_log('Starting: {}, time: {}'.format(run_name, time_now()), args)
+            set_random_seed(args.seed)
+            gamma_guidance = param_config['gamma']
+            l2_factor = param_config['L2']
+            run_name = 'net-depth: {}, Config: {}'.format(net_depth, param_config['name'])
+            write_to_log('Starting: ' + run_name + 'time: {}'.format(time_now()),  args)
 
-        # Training
-        analysis = tune.run(
-            CustomTrainer,
-            name=run_name,
-            num_samples=args.n_reps,
-            stop={"timesteps_total": args.timesteps_total},
-            config={
-                "env":args.env,
-                "num_gpus": 0.1,
-                # === Algorithm ===
-                "gamma": gamma_guidance,
-                "l2_reg_critic": l2_factor,
-                "l2_reg_actor": None,
-                # === Exploration ===
-                "learning_starts": args.learning_starts,
-                "pure_exploration_steps": args.pure_exploration_steps,
-                # # === Evaluation ===
-                # "evaluation_interval": 1 if args.smoke_test else 5,
-                # "evaluation_num_episodes": 1 if args.smoke_test else 10,
-            })
-        # Evaluation
+            critic_hiddens = [400] + [300] * (net_depth - 1)
 
-        # Get a dataframe for the last reported results of all of the trials:
-        df = analysis.dataframe(metric="episode_reward_mean")
-        mean_R[i_grid] = df['episode_reward_mean'].mean()
-        std_R[i_grid] = df['episode_reward_mean'].std()
-        # Save results so far:
-        info_dict = {'mean_R': mean_R,
-                     'std_R': std_R, 'alg_param_grid': alg_param_grid}
-        write_to_log('Finished: {}, time: {}'.format(run_name, time_now()), args)
-        write_to_log('mean_R: {}, std_R: {}'.format(mean_R[i_grid], std_R[i_grid]), args)
-        save_run_data(args, info_dict)
-
+            # Training
+            analysis = tune.run(
+                CustomTrainer,
+                name=run_name,
+                num_samples=args.n_reps,
+                stop={"timesteps_total": args.timesteps_total},
+                config={
+                    "env": args.env,
+                    "num_gpus": 0.1,
+                    # === Algorithm ===
+                    "gamma": gamma_guidance,
+                    "l2_reg_critic": l2_factor,
+                    "l2_reg_actor": None,
+                    # === Exploration ===
+                    "learning_starts": args.learning_starts,
+                    "pure_exploration_steps": args.pure_exploration_steps,
+                    "critic_hiddens": critic_hiddens,
+                })
+            # Evaluation
+            # Get a dataframe for the last reported results of all of the trials:
+            df = analysis.dataframe(metric="episode_reward_mean")
+            mean_reward = df['episode_reward_mean'].mean()
+            std_reward = df['episode_reward_mean'].std()
+            mean_R[i_net_depth, i_param_config] = mean_reward
+            std_R[i_net_depth, i_param_config] = std_reward
+            # Save results so far:
+            info_dict = {'mean_R': mean_R,
+                         'std_R': std_R, 'net_depth_grid': net_depth_grid, 'param_configs_grid':param_configs_grid}
+            write_to_log('Finished: ' + run_name + 'time: {}'.format(time_now()), args)
+            write_to_log('mean_R: {}, std_R: {}'.format(mean_reward, std_reward), args)
+            save_run_data(args, info_dict)
+        # end for i_param_config
+    # end for i_net_depth_grid
     stop_time = timeit.default_timer()
     write_to_log('Total runtime: ' +
                  time.strftime("%H hours, %M minutes and %S seconds", time.gmtime(stop_time - start_time)), args)
 
-if args.param_grid_def['type'] == 'L2_factor':
-    alg_param_grid *= 1e2
-    xlabel = 'L2 Factor (1e-2) '
-elif args.param_grid_def['type'] == 'gamma_guidance':
-    xlabel = 'Guidance Discount Factor'
-else:
-    raise ValueError('Unrecognized args.grid_type')
-
-
 ci_factor = 1.96/np.sqrt(args.n_reps)  # 95% confidence interval factor
 plt.figure()
-plt.errorbar(alg_param_grid, mean_R, yerr=std_R * ci_factor,
-             marker='.')
+for i_param_config, param_config in enumerate(param_configs_grid):
+    plt.errorbar(net_depth_grid, mean_R[:, i_param_config], yerr=std_R[:, i_param_config] * ci_factor,
+                 marker='.', label=param_config['name'])
 plt.grid(True)
-plt.xlabel(xlabel)
+plt.xlabel('Network Depth')
 # plt.ylim([2200, 3000])
 
 plt.ylabel('Average Episode Return')
