7851d8cdd8e8986302a1b01d6360d51d002bd6dd
ronamit <Was21235>
2020-01-09 11:40:52 +0200

    update linux-4

--- main_ray.py
+++ main_ray.py
@@ -7,6 +7,7 @@
 
 from __future__ import division, absolute_import, print_function
 import sys, os
+from copy import deepcopy
 from os.path import dirname, join, abspath
 project_dir = abspath(join(dirname(__file__), '..'))
 sys.path.insert(0, project_dir)
@@ -50,7 +51,7 @@ local_mode = False   # True/False - run non-parallel to get error messages and d
 save_PDF = False  # False/True - save figures as PDF file
 
 # Option to load previous run results or continue unfinished run or start a new run:
-run_mode = 'Load'   # 'New' / 'Load' / 'Continue'
+run_mode = 'Load'   # 'New' / 'Load' / 'Continue' / 'ContinueNewGrid'
 # If run_mode ==  'Load' / 'Continue' use this results dir:
 result_dir_to_load = './saved/2020_01_03_15_27_19'
 
@@ -83,6 +84,26 @@ if run_mode in {'Load', 'Continue'}:
     std_R = info_dict['std_R']
     alg_param_grid = info_dict['alg_param_grid']
     print('Loaded parameters: \n', args, '\n', '-'*20)
+
+elif run_mode == 'ContinueNewGrid':
+    # Create a new gird according to param_grid_def defined above, and use the loaded results if compatible.
+    # all the other run  args (besides param_grid_def) are according to the loaded file
+    loaded_args, info_dict = load_run_data(result_dir_to_load)
+    loaded_alg_param_grid = info_dict['alg_param_grid']
+    loaded_param_grid_def = args.param_grid_def
+    alg_param_grid = np.around(get_grid(loaded_param_grid_def), decimals=10)
+    n_gammas = len(alg_param_grid)
+    mean_R = np.full(n_gammas, np.nan)
+    std_R = np.full(n_gammas, np.nan)
+    for i_grid, alg_param in enumerate(alg_param_grid):
+        if alg_param in loaded_alg_param_grid:
+            load_idx = np.nonzero(loaded_alg_param_grid == alg_param)
+            mean_R = info_dict['mean_R'][load_idx]
+            std_R = info_dict['std_R'][load_idx]
+    args = deepcopy(loaded_args)
+    args.param_grid_def = loaded_param_grid_def
+    print('Run parameters: \n', args, '\n', '-'*20)
+
 else:
     # Start from scratch
     create_result_dir(args)
