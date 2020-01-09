2240f11f4190ec13f66d0f6d8aa3ab6550cea205
ronamit <Was21235>
2020-01-09 14:38:49 +0200

    update linux3

--- trash/main.py
+++ trash/main.py
@@ -7,7 +7,6 @@
 
 from __future__ import division, absolute_import, print_function
 import sys, os
-from copy import deepcopy
 from os.path import dirname, join, abspath
 project_dir = abspath(join(dirname(__file__), '..'))
 sys.path.insert(0, project_dir)
@@ -36,7 +35,7 @@ plt.rcParams.update(params)
 parser = argparse.ArgumentParser()
 parser.add_argument('--run-name', type=str, help='Name of dir to save results in (if empty, name by time)', default='')
 parser.add_argument('--seed', type=int,  help='random seed', default=1)
-parser.add_argument("--env", default="Ant-v2")  # OpenAI gym environment name
+parser.add_argument("--env", default="Hopper-v2")  # OpenAI gym environment name
 parser.add_argument("--default_discount", default=0.999)  # Default Discount factor
 parser.add_argument('--timesteps_total', type=int,  default=1e5)
 parser.add_argument('--learning_starts', type=int,  default=1e4)
@@ -51,15 +50,15 @@ local_mode = False   # True/False - run non-parallel to get error messages and d
 save_PDF = False  # False/True - save figures as PDF file
 
 # Option to load previous run results or continue unfinished run or start a new run:
-run_mode = 'New'   # 'New' / 'Load' / 'Continue' / 'ContinueNewGrid'
+run_mode = 'New'   # 'New' / 'Load' / 'Continue'
 # If run_mode ==  'Load' / 'Continue' use this results dir:
-result_dir_to_load = './saved/2020_01_03_15_27_19'
+result_dir_to_load = '../saved/2020_01_03_16_45_26'
 
 args.n_reps = 100   # 100 # number of experiment repetitions for each point in grid
 
 #  how to create parameter grid:
-args.param_grid_def = {'type': 'gamma_guidance', 'spacing': 'linspace', 'start': 0.995, 'stop': 0.99, 'num': 10}
-# args.param_grid_def = {'type': 'L2_factor', 'spacing': 'linspace', 'start': 0.0, 'stop': 0.02, 'num': 21}
+args.param_grid_def = {'type': 'gamma_guidance', 'spacing': 'linspace', 'start': 0.95, 'stop': 0.999, 'num': 15}
+# args.param_grid_def = {'type': 'L2_factor', 'spacing': 'linspace', 'start': 0.0, 'stop': 0.05, 'num': 21}
 # args.param_grid_def = {'type': 'L2_factor', 'spacing': 'list', 'list': [0, 1e-5, 2e-5, 3e-5, 4e-5, 5e-5, 1e-4]}
 
 gamma_guidance = args.default_discount# default discount factor for algorithm
@@ -71,39 +70,21 @@ if smoke_test:
     args.n_reps = 2
     args.timesteps_total = 1e2
     args.learning_starts = 1e1
-    args.pure_exploration_steps =1e1
+    args.pure_exploration_steps = 1e1
 
 # ---------------------------------------------------------------------------------------------------------------------------------#
 #  Get results
 # ---------------------------------------------------------------------------------------------------------------------------------#
-
+run_time = 0
 if run_mode in {'Load', 'Continue'}:
     #  Load previous run
     args, info_dict = load_run_data(result_dir_to_load)
     mean_R = info_dict['mean_R']
     std_R = info_dict['std_R']
     alg_param_grid = info_dict['alg_param_grid']
+    result_reward_mat = info_dict['result_reward_mat']
+    run_time = info_dict['run_time']
     print('Loaded parameters: \n', args, '\n', '-'*20)
-
-elif run_mode == 'ContinueNewGrid':
-    # Create a new gird according to param_grid_def defined above, and use the loaded results if compatible.
-    # all the other run  args (besides param_grid_def) are according to the loaded file
-    loaded_args, info_dict = load_run_data(result_dir_to_load)
-    loaded_alg_param_grid = info_dict['alg_param_grid']
-    loaded_param_grid_def = args.param_grid_def
-    alg_param_grid = np.around(get_grid(loaded_param_grid_def), decimals=10)
-    n_gammas = len(alg_param_grid)
-    mean_R = np.full(n_gammas, np.nan)
-    std_R = np.full(n_gammas, np.nan)
-    for i_grid, alg_param in enumerate(alg_param_grid):
-        if alg_param in loaded_alg_param_grid:
-            load_idx = np.nonzero(loaded_alg_param_grid == alg_param)
-            mean_R = info_dict['mean_R'][load_idx]
-            std_R = info_dict['std_R'][load_idx]
-    args = deepcopy(loaded_args)
-    args.param_grid_def = loaded_param_grid_def
-    print('Run parameters: \n', args, '\n', '-'*20)
-
 else:
     # Start from scratch
     create_result_dir(args)
@@ -111,6 +92,8 @@ else:
     n_gammas = len(alg_param_grid)
     mean_R = np.full(n_gammas, np.nan)
     std_R = np.full(n_gammas, np.nan)
+    n_grid = len(alg_param_grid)
+    result_reward_mat = np.full((n_grid, args.n_reps), np.nan)
 
 if run_mode in {'New', 'Continue'}:
     # Run grid
@@ -133,30 +116,40 @@ if run_mode in {'New', 'Continue'}:
 
         write_to_log('Starting: {}, time: {}'.format(run_name, time_now()), args)
 
-        # Training
-        analysis = tune.run(
-            CustomTrainer,
-            name=run_name,
-            num_samples=args.n_reps,
-            stop={"timesteps_total": args.timesteps_total},
-            config={
-                "env":args.env,
-                "num_gpus": 0.1,
-                # === Algorithm ===
-                "gamma": gamma_guidance,
-                "l2_reg_critic": l2_factor,
-                "l2_reg_actor": None,
-                # === Exploration ===
-                "learning_starts": args.learning_starts,
-                "pure_exploration_steps": args.pure_exploration_steps,
-                # # === Evaluation ===
-                # "evaluation_interval": 1 if args.smoke_test else 5,
-                # "evaluation_num_episodes": 1 if args.smoke_test else 10,
-            })
-        # Evaluation
+        for i_rep in range(args.n_reps):
+            seed = args.seed + i_rep
+            # Training
+            analysis = tune.run(
+                CustomTrainer,
+                name=run_name,
+                num_samples=1,
+                random_state_seed=2,
+                stop={"timesteps_total": args.timesteps_total},
+                config={
+                    "env":args.env,
+                    "num_gpus": 0.1,
+                    # === Algorithm ===
+                    "gamma": gamma_guidance,
+                    "l2_reg_critic": l2_factor,
+                    "l2_reg_actor": None,
+                    # === Exploration ===
+                    "learning_starts": args.learning_starts,
+                    "pure_exploration_steps": args.pure_exploration_steps,
+                    # # === Evaluation ===
+                    # "evaluation_interval": 1 if args.smoke_test else 5,
+                    # "evaluation_num_episodes": 1 if args.smoke_test else 10,
+                })
+            # Evaluation
+            # Get a dataframe for the last reported results of all of the trials:
+            df = analysis.dataframe(metric="episode_reward_mean")
+            result_reward_mat[i_grid, i_rep] = df['episode_reward_mean'][0]
+            # Save results so far:
+            stop_time = timeit.default_timer()
+            run_time += stop_time - start_time
+            start_time = timeit.default_timer()
+            save_run_data(args, {'alg_param_grid': alg_param_grid, 'result_reward_mat': result_reward_mat,
+                                 'run_time': run_time}, verbose=0)
 
-        # Get a dataframe for the last reported results of all of the trials:
-        df = analysis.dataframe(metric="episode_reward_mean")
         mean_R[i_grid] = df['episode_reward_mean'].mean()
         std_R[i_grid] = df['episode_reward_mean'].std()
         # Save results so far:
@@ -185,7 +178,6 @@ plt.errorbar(alg_param_grid, mean_R, yerr=std_R * ci_factor,
              marker='.')
 plt.grid(True)
 plt.xlabel(xlabel)
-# plt.ylim([2200, 3000])
 
 plt.ylabel('Average Episode Return')
 if save_PDF:
